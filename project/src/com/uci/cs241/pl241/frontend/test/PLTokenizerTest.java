530e5390a829038f540a3b94cf0df35e3e231b53
chris-wood <christopherwood07@gmail.com>
2014-01-13 17:58:11 -0800

    Added test package for the frontend - need to write the Tokenizer tests

---
+++ project/src/com/uci/cs241/pl241/frontend/test/PLTokenizerTest.java
@@ -0,0 +1,16 @@
+package com.uci.cs241.pl241.frontend.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PLTokenizerTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
